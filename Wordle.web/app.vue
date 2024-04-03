64d9280d5d152b5c80cbce5d848d8d785277f431
Grant Erickson <grant@granterickson.com>
2024-04-02 20:45:29 -0700

    Add Vuetify

--- Wordle.web/app.vue
+++ Wordle.web/app.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <h1>Wordle</h1>
+  <v-container>
+    <v-card width="300" elevation="4">
+      <v-card-title> <h1>Wordle</h1> </v-card-title>
+      <v-card-text>
+        <div v-for="guess of game.guesses" :key="guess.text">
+          <Word :word="guess" />
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-text-field label="guess" v-model="guess" v-on:keyup.enter="onEnter" />
+        <v-btn size="large" color="purple" variant="tonal" @click="submitGuess" class="ml-4 mb-4" prepend-icon="mdi-location-enter">Submit</v-btn>
+      </v-card-actions> 
+    </v-card>
 
-    <div v-for="guess of game.guesses" :key="guess.text">
-      <Word :word="guess" />
-    </div>
-
-    <input v-model="guess" v-on:keyup.enter="onEnter" />
-    <button @click="submitGuess">Submit</button>
-  </div>
+  </v-container>
 </template>
 
 <script setup lang="ts">
--- Wordle.web/nuxt.config.ts
+++ Wordle.web/nuxt.config.ts
@@ -2,4 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/test-utils/module"],
+  build: {
+    transpile: ['vuetify'],
+  },
+  css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css',],
 });
